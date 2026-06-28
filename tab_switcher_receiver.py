0b3914feb2bfe9101fdeaf660a6fbe7acb269393
Brad Urbiztondo <bradurbiz@gmail.com>
2026-06-28 17:59:46 -0400

    Add tab_switcher_receiver.py

---
+++ tab_switcher_receiver.py
@@ -0,0 +1,38 @@
+import socket
+import pyautogui
+import time
+
+port = 1
+
+def switch_tab():
+    time.sleep(0.1)
+    pyautogui.hotkey("ctrl", "tab")
+    print("Tab Switched")
+
+def main():
+    server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    server_sock.bind(("", port))
+    server_sock.listen(1)
+
+    print("...")
+
+    while True:
+        try:
+            client_sock, addr = server_sock.accept()
+            print(f"Connected: {addr}")
+
+            data = client_sock.recv(64).decode("utf-8").strip()
+            print(f"Received: {data}")
+
+            if data == "SWITCH":
+                switch_tab()
+
+            client_sock.close()
+
+        except KeyboardInterrupt:
+            print("Terminated")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+
+    server_sock.close()
